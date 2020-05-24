3b938e98832367a0f0eadcaccd7a19a6b3376524
jon.entwistle <jon.entwistle@netcentric.biz>
2020-05-24 14:52:30 +0200

    CI tests

--- src/io/fabric8/Utils.groovy
+++ src/io/fabric8/Utils.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/groovy
 package io.fabric8
 
+@Grab(group='io.fabric8', module='openshift-client', version='2.2.13')
 @Grab(group='io.fabric8', module='kubernetes-client', version='2.2.13')
 import com.cloudbees.groovy.cps.NonCPS
 import io.fabric8.kubernetes.client.DefaultKubernetesClient
